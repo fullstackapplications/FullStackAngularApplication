4ab3b10f1ca8299769e8376905b65361a79bb8af
Help <pczerokey@gmail.com>
2019-07-19 16:11:02 -0400

    get and post to same endpoint

--- backend/server.js
+++ backend/server.js
@@ -1,36 +1,46 @@
 const express = require('express');
-
-
 const bodyParser = require('body-parser'); //middle ware to read body sent
 const app = express();
 
-const port = 1234;
+const port = 63145;
 
 messages = [
     {text: 'some text', owner: 'Tim'},
     {text: 'other text', owner: 'Jane'}
 ];
 
-
-
 app.use(bodyParser.json()); // lets express know what we receive in our body should be in json
-
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
 
-app.get('/messages', (request, response) => {
+var api = express.Router()
+
+// app.get('/messages', (request, response) => {
+// //     response.json(messages);    //displayed on the page
+// //     console.log("Someone connected to messages page!");
+// // })
+// //
+// // app.post('/message', (request, response) => {
+// //     console.log(request.body);
+// //     messages.push(request.body);
+// //     response.sendStatus(200);       //sent to client
+// // })
+
+api.get('/messages', (request, response) => {
     response.json(messages);    //displayed on the page
     console.log("Someone connected to messages page!");
 })
 
-app.post('/message', (request, response) => {
+api.post('/messages', (request, response) => {
     console.log(request.body);
     messages.push(request.body);
     response.sendStatus(200);       //sent to client
 })
+
+app.use('/api', api);
 
 app.listen( port , () => {
     console.log(`Listening on port ${port}!`);
