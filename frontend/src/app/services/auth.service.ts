3d7bb2f1ddbacc3748a564c9b2efc710f90c5a80
Help <pczerokey@gmail.com>
2019-07-24 15:52:20 -0400

    Added redirect and authentication check

--- frontend/src/app/services/auth.service.ts
+++ frontend/src/app/services/auth.service.ts
@@ -1,20 +1,41 @@
 import { Injectable } from '@angular/core';
 import {HttpClient}   from '@angular/common/http';
+import {Router}       from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-
   BASE_URL = 'http://localhost:63145/auth/';
+  NAME_KEY = 'name';
+  TOKEN_KEY = 'token';
 
-  constructor(private http: HttpClient) { }
+
+  constructor(private http: HttpClient,
+              private router: Router) { }
+
+  get isAuthenticated() {
+    return !!localStorage.getItem(this.TOKEN_KEY);
+  }
+
+  get name() {
+    return localStorage.getItem(this.NAME_KEY);
+  }
 
   register(user) {
     delete user.confirmPassword;
     this.http.post(this.BASE_URL + 'register', user).subscribe( response => {
+      const authResponse = response;
       // @ts-ignore
-      localStorage.setItem('token', response.token);
+      if (!authResponse.token) {
+        return null;
+      }
+      // @ts-ignore
+      localStorage.setItem(this.TOKEN_KEY, authResponse.token);
+      // @ts-ignore
+      localStorage.setItem(this.NAME_KEY, authResponse.firstName);
+
+      this.router.navigate(['/']);
     });
   }
 }
